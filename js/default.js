c79ed08196bba60ccbd5146a57f4334a63218d66
紅月九音 <teiruzuof.15@gmail.com>
2017-07-19 17:20:08 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,6 @@
+var thmubs = document.querySelectorALL('.thumb);
+for(idx in thmubs){
+    thmubs[idx].onclick = function(){
+          document.getElementById("bigimg").src = 'img/' + this.dataset.image+ '.jpg';
+     }
+}
